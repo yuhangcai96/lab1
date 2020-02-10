7718c12b6b2ec87d171004c787be6b73dfbe0aed
Danny <Danny@DESKTOP-N7VQ5AH>
2020-02-10 15:52:02 -0600

    add lab1 taxsale

---
+++ lab1/src/main/java/taxsale.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Danny
+ */
+public class taxsale {
+     public static void main(String[] args)
+    {
+       Scanner keyboard = new Scanner(System.in);
+        
+        System.out.println("Enter the amount of a purchase");
+        
+        float theAmountofapurchase= keyboard.nextFloat();
+        
+        float thestatesalesTax = (float) ( 0.04*theAmountofapurchase);
+        
+        float thecountysalesTax = (float) (0.02*theAmountofapurchase);
+        
+        float thetotalsalesTax = (thestatesalesTax+thecountysalesTax);
+        
+        float thetotaloftheSale = (theAmountofapurchase+thestatesalesTax+thecountysalesTax);
+        
+        System.out.println("The amount of the purchase: "+ theAmountofapurchase);
+        
+        System.out.println("The state sales tax: "+thestatesalesTax);
+        
+        System.out.println("The county sales tax: "+thecountysalesTax);
+        
+        System.out.println("The total sales tax: "+ thetotalsalesTax);
+        
+        System.out.println("The total of the sale: "+ thetotaloftheSale);
+}
