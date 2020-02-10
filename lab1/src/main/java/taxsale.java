7df20406381d47060fff676525a707bde8e52b95
Danny <Danny@DESKTOP-N7VQ5AH>
2020-02-10 16:02:15 -0600

    add lab1 taxsale

--- lab1/src/main/java/taxsale.java
+++ lab1/src/main/java/taxsale.java
@@ -1,3 +1,6 @@
+
+import java.util.Scanner;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -34,4 +37,5 @@ public class taxsale {
         System.out.println("The total sales tax: "+ thetotalsalesTax);
         
         System.out.println("The total of the sale: "+ thetotaloftheSale);
+}
 }
